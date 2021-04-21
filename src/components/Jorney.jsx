aa7d67b4d5174adae094edc244050a15fec7749c
Roman <roman.pohribniak@gmail.com>
2021-04-22 00:18:21 +0300

    Full version

--- src/components/Jorney.jsx
+++ src/components/Jorney.jsx
@@ -1,13 +1,7 @@
 import GalleryItem from "./GalleryItem";
-import img1 from "../images/gallery1.jpg";
-import img2 from "../images/gallery2.jpg";
-import img3 from "../images/gallery3.jpg";
-import img4 from "../images/gallery4.jpg";
-import img5 from "../images/gallery5.jpg";
-import img6 from "../images/gallery6.jpg";
 
 export default function Jorney() {
-  const images =[{header:"Santorini, Greece",image:img1},{header:"Vernazza, Italy",image:img2},{header:"San Francisco",image:img3},{header:"Navagio, Greece",image:img4},{header:"Ao Nang, Thailand",image:img5},{header:"Phi Phi Islands, Thailand",image:img6}];
+  const images =[{header:"Santorini, Greece",image:"//images01.nicepage.io/28/58/2858f3748a025304669419b5dbd89dd3.jpeg"},{header:"Vernazza, Italy",image:"//images01.nicepage.io/9e/af/9eaf6a4d06c28cd9596d0b792f4780b5.jpeg"},{header:"San Francisco",image:"//images01.nicepage.io/2f/c9/2fc963db2b8f554519a2b21cc60d75a3.jpeg"},{header:"Navagio, Greece",image:"//images01.nicepage.io/ef/2a/ef2a13068362a84ceee3efa5eaab583b.jpeg"},{header:"Ao Nang, Thailand",image:"//images01.nicepage.io/29/8d/298d0a5cd8893ec3a947289a7347c9f0.jpeg"},{header:"Phi Phi Islands, Thailand",image:"//images01.nicepage.io/6e/8c/6e8c81f6a006352a225d163d742b148c.jpeg"}];
   return (
     <section className="jorney">
       <div className="container">
