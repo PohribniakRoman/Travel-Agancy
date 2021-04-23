e52ace9f939d49eabd42fb24805ad740eed8978c
Roman <roman.pohribniak@gmail.com>
2021-04-23 22:52:32 +0300

    links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ export default function Footer() {
     return <section className="footer">
         <h1>Travel agancy</h1>
         <ul>
-            <li><i className="fab fa-facebook-f"></i></li>
-            <li><i className="fab fa-twitter"></i></li>
-            <li><i className="fab fa-instagram"></i></li>
-            <li><i className="fab fa-linkedin-in"></i></li>
+            <li><a href="/"><i className="fab fa-facebook-f"></i></a></li>
+            <li><a href="/"><i className="fab fa-twitter"></i></a></li>
+            <li><a href="/"><i className="fab fa-instagram"></i></a></li>
+            <li><a href="/"><i className="fab fa-linkedin-in"></i></a></li>
         </ul>
         <p>Travel makes one modest. You see what a tiny place you occupy in the world.</p>
     </section>
